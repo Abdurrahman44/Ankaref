7691c8590b87351850edd6840f64aac8b57f9375
gokta <goktasabdurrahman44@gmail.com>
2023-12-06 19:24:56 +0300

    m

--- src/main/java/com/example/ankaref/Mapper/olld/ModelMapperService.java
+++
@@ -1,8 +0,0 @@
-//package com.example.ankaref.Mapper.olld;
-//
-//import org.modelmapper.ModelMapper;
-//
-//public interface ModelMapperService {
-//    ModelMapper forRequest();
-//    ModelMapper forResponse();
-//}
